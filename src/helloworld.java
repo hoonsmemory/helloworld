80cf814f2791ad98fbfd34e7d0b4bd3d1a541e22
BIT <BIT@DESKTOP-08BCIT0>
2019-08-05 17:59:29 +0900

    commit

--- src/helloworld.java
+++ src/helloworld.java
@@ -6,6 +6,8 @@ public class helloworld {
 		System.out.printf("Hello World");
 		System.out.printf("Hello World");
 		System.out.printf("Hello World");
+		System.out.printf("Hello World");
+		System.out.printf("Hello World");
 	}
 
 }
